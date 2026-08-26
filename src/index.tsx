54fbf8baa8c659523c5815c083b333e35b52b9ec
Vault505 <201951138+Vault505@users.noreply.github.com>
2026-08-26 15:00:24 +0200

    Erste Farbschema aktualisiert

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,22 @@ import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import App from './App';
 import './index.css';
 
+// ⭐ 1. Dein eigenes Semtation-Theme definieren
+const semtationTheme = {
+  ...webLightTheme,
+  colorBrandForeground1: "#1F4FA8",
+  colorBrandBackground: "#1F4FA8",
+  colorBrandBackgroundHover: "#4A7CC5",
+  colorNeutralForeground1: "#000000",
+  colorNeutralBackground1: "#FFFFFF",
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <FluentProvider theme={webLightTheme}>
+    {/* ⭐ 2. Hier statt webLightTheme dein eigenes Theme verwenden */}
+    <FluentProvider theme={semtationTheme}>
       <App />
     </FluentProvider>
   </React.StrictMode>
